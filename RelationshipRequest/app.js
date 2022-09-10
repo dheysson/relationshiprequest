4208e8b1ebe0844cab3ae75a272198388b69d6cd
Dheysson Silva <dheyssonsousasilva@gmail.com>
2022-09-10 16:53:44 -0300

    second commit

--- RelationshipRequest/app.js
+++ RelationshipRequest/app.js
@@ -1,10 +1,12 @@
 window.onload = function() {
   const parent = document.querySelector('#sky')
+  var heartColor = '#fff'
 
   function instantiateHeart() {
     const heart = document.createElement('p')
     heart.innerHTML = '❤'
     heart.classList.add('heart')
+    heart.style.color = heartColor
     heart.style.left = Math.random() * 100 + 'vw'
     heart.style.transform = `rotateZ(${Math.floor(Math.random() * 360)}deg)`
     heart.style.fontSize = Math.random() * 25 + 'px'
@@ -14,16 +16,23 @@ window.onload = function() {
 
   setInterval(instantiateHeart, 50)
 
-  const button = document.querySelectorAll('button')[1]
-
-  button.addEventListener('mouseover', () => {
-    button.style.left = Math.round(Math.random() * 80) + '%' 
-    button.style.top = Math.round(Math.random() * 80) + '%' 
+  const noButton = document.querySelectorAll('button')[1]
+  noButton.addEventListener('mouseover', () => {
+    noButton.style.left = Math.round(Math.random() * 80) + '%' 
+    noButton.style.top = Math.round(Math.random() * 80) + '%' 
   })
 
-  button.addEventListener('mousedown', () => {
-    button.style.left = Math.round(Math.random() * 80) + '%' 
-    button.style.top = Math.round(Math.random() * 80) + '%' 
+  noButton.addEventListener('click', () => {
+    noButton.style.left = Math.round(Math.random() * 80) + '%' 
+    noButton.style.top = Math.round(Math.random() * 80) + '%' 
   })
   
+  const yesButton = document.querySelectorAll('button')[0]
+  yesButton.addEventListener('click', () => {
+    document.querySelector('span').innerHTML = 'This was you best choice you ever had!'
+    yesButton.remove()
+    noButton.remove()
+    heartColor = '#f00'
+    document.querySelector('iframe').style.display = 'inherit'
+  })
 }
